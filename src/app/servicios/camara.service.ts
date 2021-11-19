d39d8e9bb438913b33c441dfa17a696d076cbd11
Nora <nsansaturnino@birt.eus>
2021-11-19 10:35:09 +0100

    Guardar y recuperar imágenes

--- src/app/servicios/camara.service.ts
+++ src/app/servicios/camara.service.ts
@@ -1,3 +1,7 @@
+import { Capacitor } from '@capacitor/core';
+import { Platform } from '@ionic/angular';
+import { Filesystem, Directory } from '@capacitor/filesystem';
+import { GestionStorageService } from './gestion-storage.service';
 import { Injectable } from '@angular/core';
 import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
 
@@ -7,8 +11,40 @@ import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
 export class CamaraService {
 
   listaUrl: string[] = [];
+  listaPath: string[]= [];
 
-  constructor() { }
+  constructor(private platform: Platform, private usarStorage: GestionStorageService) {
+    this.loadSaved();
+   }
+
+   private async loadSaved() {
+    // Recupera los datos de Storage en formato string
+    const photoList = await this.usarStorage.getObject("rutas");
+
+    // Lo parsea a un array de objetos IFoto y lo almacena en el atributo accesoFotos
+    // Si no obtiene datos inicializará el array para poder empezar a almacenar fotos.
+    this.listaPath = JSON.parse(photoList.value) || [];    
+    
+    // Se obtiene una URL válida para visualizar cada foto guardada en el sistema de archivos
+    // En los navegadores debemos cargar las imágenes en formato base64 para que se visualicen correctamente
+    // En los dispositivos podemos obtener la URL a partir del path
+
+    let webviewPath: string;
+    for (let photo of this.listaPath) {
+      if (!this.platform.is("hybrid")) {     
+          const readFile = await Filesystem.readFile({
+              path: photo,
+              directory: Directory.Data
+          });
+
+          // La URL de la foto en base64 para que se pueda visualizar
+          webviewPath = `data:image/jpeg;base64,${readFile.data}`;      
+      } else {
+        webviewPath = Capacitor.convertFileSrc(photo);
+      }
+      this.listaUrl.push(webviewPath);
+    }
+  }
 
   getListaUrl(): string[] {
     return this.listaUrl;
@@ -18,18 +54,42 @@ export class CamaraService {
     const image = await Camera.getPhoto({
       quality: 90,
       allowEditing: false,
-      resultType: CameraResultType.Uri,
+      resultType: CameraResultType.Base64,
       source: CameraSource.Camera
     });
 
-    console.log(image);
+    // Guardamosla URL para visualizar la imagen añadiendo la cabecera base64
+    this.listaUrl.push("data:image/jpeg;base64," + image.base64String);
 
-    // image.webPath will contain a path that can be set as an image src.
-    // You can access the original file using image.path, which can be
-    // passed to the Filesystem API to read the raw data of the image,
-    // if desired (or pass resultType: CameraResultType.Base64 to getPhoto)
-    this.listaUrl.push(image.webPath);
+    this.savePicture(image);
 
+  }
 
-  };
+  private async savePicture(image) {
+
+    let base64Data = image.base64String;
+    // Guarda el fichero
+    const fileName = new Date().getTime() + '.jpeg';
+    const savedFile = await Filesystem.writeFile({
+      path: fileName,
+      data: base64Data,
+      directory: Directory.Data
+    });
+    
+    // Si trabajamos con el navegador, guardamos solo el nombre del fichero. Lo necesitaremos para 
+    // Con dispositivos móviles guardaremos la ruta que nos devuelve
+    let ruta: string;
+  
+    if (this.platform.is('hybrid')) {
+      ruta = savedFile.uri;
+    } else {
+      ruta = fileName;
+  
+    }
+  
+    this.listaPath.push(ruta);
+  
+    // Almacenamos los datos de las fotos en Storage para poder acceder a ellas
+    this.usarStorage.setObject("rutas", this.listaPath);
+  }
 }

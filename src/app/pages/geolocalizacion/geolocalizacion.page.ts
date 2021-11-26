5c08a5c5e999db5da6f9372d8e0c53ac2c69e28a
Nora <nsansaturnino@birt.eus>
2021-11-26 09:03:46 +0100

    Maps JavaScript API

--- src/app/pages/geolocalizacion/geolocalizacion.page.ts
+++ src/app/pages/geolocalizacion/geolocalizacion.page.ts
@@ -7,20 +7,50 @@ import { Geolocation } from '@capacitor/geolocation';
   styleUrls: ['./geolocalizacion.page.scss'],
 })
 export class GeolocalizacionPage implements OnInit {
+  public map: google.maps.Map;
 
   constructor() { }
 
   ngOnInit() {
+    this.initMap();
   }
 
-  private async printCurrentPosition() {
+  private async obtenerPosicion() {
     
     const coordinates = await Geolocation.getCurrentPosition();
 
     console.log('Current position:', coordinates);
+
+    return coordinates;
   }
 
   public onClick() {
-    this.printCurrentPosition();
+    this.obtenerPosicion();
+  }
+
+  public async initMap() {
+
+    let coordenadas = await this.obtenerPosicion();
+    console.log("Posición acctual:", coordenadas);
+
+    // Obtenemos la latitud y la longitud
+    const posicion = {
+      lat: coordenadas.coords.latitude,
+      lng: coordenadas.coords.longitude
+    };
+
+    // Mostramos el mapa centrado en la posición almacenada
+    const map = new google.maps.Map(document.getElementById("map") as HTMLElement,
+      {
+        zoom: 6,
+        center: posicion
+      }
+    );
+
+    // Mostramos el marcador en la misma posición
+    const marker = new google.maps.Marker({
+      position: posicion,
+      map: map,
+    });
   }
 }
